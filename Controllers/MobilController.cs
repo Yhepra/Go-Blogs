15cd48b561fbe84769efd772d42180c1ad92b78b
Yoga Hendrapratama <yhepra@gmail.com>
2022-01-01 20:30:08 +0700

    Add project files.

---
+++ Controllers/MobilController.cs
@@ -0,0 +1,44 @@
+﻿using Go_Blogs.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace Go_Blogs.Controllers
+{
+    public class MobilController : Controller
+    {
+        public IActionResult Index()
+        {
+            //var mobils = new List<Mobil>();
+
+            //mobils.Add(new Mobil
+            //{
+            //    IdRegistrasi = 1,
+            //    Tipe = "Sedan",
+            //    Merk = "Toyota",
+            //    Varian = "TF86"
+            //});
+
+            //mobils.Add(new Mobil
+            //{
+            //    IdRegistrasi = 2,
+            //    Tipe = "SUV",
+            //    Merk = "",
+            //    Varian = "Naga"
+            //});
+
+            string nama = "Yoga";
+            ViewBag.namaSaya = nama;
+            //ViewBag.mobilnya = mobils;
+            var banyakMobil = new Mobil[]
+            {
+                new Mobil{IdRegistrasi = 1, Tipe="Sedan", Merk = "Toyota", Varian = "FT86"},
+                new Mobil{IdRegistrasi = 2, Tipe="SUV", Merk = "Honda", Varian = "RAV4"},
+                new Mobil{IdRegistrasi = 3, Tipe="Sedan", Merk = "BMW", Varian = "Accord"},
+                new Mobil{IdRegistrasi = 4, Tipe="SUV", Merk = "Toyota", Varian = "CRV"},
+                new Mobil{IdRegistrasi = 5, Tipe="Sedan", Merk = "Lambo", Varian = "City"},
+            };
+
+            return View(banyakMobil);
+        }
+    }
+}
---
+++ Models/Mobil.cs
@@ -0,0 +1,10 @@
+﻿namespace Go_Blogs.Models
+{
+    public class Mobil
+    {
+        public int IdRegistrasi { get; set; }
+        public string Tipe { get; set; }
+        public string Merk { get; set; }
+        public string Varian { get; set; }
+    }
+}
